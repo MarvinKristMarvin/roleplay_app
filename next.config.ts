a9976f2b63444f974aa996018345576aa56ec54e
MarvinKristMarvin <166423247+MarvinKristMarvin@users.noreply.github.com>
2025-03-19 18:35:04 +0100

    changed port config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {},
+  output: "standalone", // Ensures Render can run Next.js properly
 };
 
 export default nextConfig;
